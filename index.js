6db6c789687d0225c9e89eae8f61582635b22ef2
rasmus.aer@gmail.com <b5RUaPmzGr5M>
2021-06-12 14:33:56 +0300

    remove leftovers from repl.it

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
-const keepAlive = require("./server")
 
 const MODPACK_NOTIFICATION = "Link to latest Valheim modpack: " + process.env['MODPACK_URL'] + "\n\n Installation instructions: Delete existing BepinEx folder from Valheim root folder (usually: C:\\Program Files (x86)\\steam\\steamapps\\common\\Valheim). Extract zip into Valheim root folder overwriting everything else.";
 
@@ -15,5 +14,4 @@ client.on("message", msg => {
   }
 })
 
-keepAlive()
 client.login(process.env['DISCORD_TOKEN'])
